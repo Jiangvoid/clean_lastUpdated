82feedb9c49d644dd04a95489b956243c7890407
Jiangvoid <jiangp@fusionskye.com>
2018-08-03 10:28:50 +0800

    Initial commit

---
+++ src/main/java/DelEmptyFolders.java
@@ -0,0 +1,88 @@
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+/**
+ *
+ * **
+ *  * Jiang
+ *  * 2018/5/3 16:21
+ *  *
+ * Description：递归方式，扫描并删除磁盘中的空文件夹
+ *（C盘中的一些空文件夹是系统文件夹，删除多次后空文件夹数量不变，则停止此程序）
+ * */
+public class DelEmptyFolders {
+    //空文件夹的绝对路径
+    private static StringBuffer paths;
+    //本次扫描的空文件夹的数量
+    private static int cnt;
+    public static void main(String[] args) {
+        boolean flag = true;
+        do{
+            cnt = 0;
+            paths = new StringBuffer();
+            long start = new Date().getTime();
+            System.out.println("正在扫描......");
+            //要扫描的磁盘
+            File disk = new File("/Users/Jiang/.m2/repository/");
+            //日志文件的位置
+            File log = new File("/Users/Jiang/Desktop/log/");
+            try {
+                //扫描磁盘
+                scanEmptyFolders(disk);
+                //空文件夹数大于0时，将文件夹的绝对路径记录到日志中并再扫描一次；否则停止扫描
+                if(cnt > 0){
+                    fileWrite(paths.toString(), log);
+                }else{
+                    flag = false;
+                }
+            } catch (FileNotFoundException e1) {
+                e1.printStackTrace();
+            } catch (IOException e2) {
+                e2.printStackTrace();
+            }
+            long end = new Date().getTime();
+            System.out.println("本次扫描完毕，耗时："+(end-start)/1000+" 秒，共删除："+cnt+" 个空文件夹！\n");
+        }while(flag);
+    }
+    /**
+     * TODO:递归扫描空文件夹
+     * @throws UnsupportedEncodingException
+     * */
+    private static void scanEmptyFolders(File file) throws UnsupportedEncodingException{
+        if(file != null && file.isDirectory()){
+            File[] files = file.listFiles();
+            //非空文件夹
+            if(files != null){
+                if(files.length > 0){
+                    for (File temp : files) {
+                        scanEmptyFolders(temp);
+                    }
+                }else{
+                    System.out.println(file.getAbsolutePath());
+                    //记录日志
+                    paths.append(new String((file.getAbsolutePath()+"\r\n").getBytes(),"UTF-8"));
+                    cnt++;
+                    //删除空文件夹
+                    file.delete();
+                }
+            }
+        }
+    }
+    /**
+     * TODO:将字符串写入文本文件
+     * @throws IOException
+     * */
+    private static void fileWrite(String info,File file) throws IOException{
+        FileOutputStream fos = new FileOutputStream(file);
+        BufferedOutputStream bos = new BufferedOutputStream(fos);
+        bos.write(info.getBytes());
+        bos.flush();
+        bos.close();
+        fos.close();
+    }
+}
---
+++ src/main/java/DelLastUpdated.java
@@ -0,0 +1,54 @@
+import java.io.File;
+import java.io.FilenameFilter;
+
+import org.apache.commons.io.filefilter.FileFilterUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * @author jiang
+ */
+public class DelLastUpdated {
+
+    private static PropertyHelper propHelper = new PropertyHelper("config");
+    private static final String KEY_MAVEN_REPO = "maven.repo";
+    private static final String MAVEN_REPO_PATH = propHelper
+            .getValue(KEY_MAVEN_REPO);
+    private static final String FILE_SUFFIX = "lastUpdated";
+    private static final Log _log = LogFactory.getLog(DelLastUpdated.class);
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        File mavenRep = new File(MAVEN_REPO_PATH);
+        if (!mavenRep.exists()) {
+            _log.warn("Maven仓库不存在！");
+            return;
+        }
+        File[] files = mavenRep.listFiles((FilenameFilter) FileFilterUtils
+                .directoryFileFilter());
+        delFileRecr(files, null);
+        _log.info("清除lastUpdated文件结束！");
+    }
+
+    private static void delFileRecr(File[] dirs, File[] files) {
+        if (dirs != null && dirs.length > 0) {
+            for (File dir : dirs) {
+                File[] childDir = dir.listFiles((FilenameFilter) FileFilterUtils
+                        .directoryFileFilter());
+                File[] childFiles = dir.listFiles((FilenameFilter) FileFilterUtils
+                        .suffixFileFilter(FILE_SUFFIX));
+                delFileRecr(childDir, childFiles);
+            }
+        }
+        if (files != null && files.length > 0) {
+            for (File file : files) {
+                if (file.delete()) {
+                    _log.info("文件: [" + file.getName() + "] 已经被删除！");
+                }
+            }
+        }
+    }
+
+}
